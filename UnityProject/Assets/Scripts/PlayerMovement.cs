d2c228ba421af1deba220558025c94f407d89f7b
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2019-12-28 17:23:24 +0100

    Basic Player Movement and Camera

---
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+
+    float x;
+    float z;
+    bool isGrounded;
+
+    Vector3 move;
+    Vector3 velocity;
+	
+	void Update ()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);        
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+	}
+}
