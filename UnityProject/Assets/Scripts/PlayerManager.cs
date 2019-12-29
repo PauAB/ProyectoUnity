e28a7c6e974a192c775379e0ad12b73498ddd1a1
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2019-12-29 22:01:21 +0100

    3D Arrow Model And Arrow Selection

--- UnityProject/Assets/Scripts/PlayerManager.cs
+++ UnityProject/Assets/Scripts/PlayerManager.cs
@@ -7,16 +7,33 @@ public class PlayerManager : MonoBehaviour {
 
     public Camera playerCam;
     public GameObject arrowPrefab;
-    public Transform arrowSpawn;    
+    public GameObject arrowFirePrefab;
+    public GameObject arrowHookPrefab;
+    public GameObject arrowSteelPrefab;
+    public Transform arrowSpawn;
     public Slider chargeSlider;
+    public Image arrowFireUI;
+    public Image arrowHookUI;
+    public Image arrowSteelUI;
 
     public float speed = 20f;
     public float chargeMax;
     public float chargeSpeedRate;    
 
     float chargeCurrent;
+    Color newColor;
 
-	void Update ()
+    void Start()
+    {
+        newColor = arrowFireUI.color;
+        newColor.a = 0.1f;
+
+        arrowFireUI.color = newColor;
+        arrowHookUI.color = newColor;
+        arrowSteelUI.color = newColor;
+    }
+
+    void Update ()
     {        
         if (Input.GetMouseButton(1) && chargeCurrent < chargeMax)
         {
@@ -34,5 +51,33 @@ public class PlayerManager : MonoBehaviour {
             chargeCurrent = 0f;
             chargeSlider.value = chargeCurrent;
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))            
+        {
+            arrowPrefab = arrowFirePrefab;
+            newColor.a = 1f;
+            arrowFireUI.color = newColor;
+            newColor.a = 0.1f;
+            arrowHookUI.color = newColor;
+            arrowSteelUI.color = newColor;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            arrowPrefab = arrowHookPrefab;
+            newColor.a = 1f;
+            arrowHookUI.color = newColor;
+            newColor.a = 0.1f;
+            arrowFireUI.color = newColor;
+            arrowSteelUI.color = newColor;
+        }            
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            arrowPrefab = arrowSteelPrefab;
+            newColor.a = 1f;
+            arrowSteelUI.color = newColor;
+            newColor.a = 0.1f;
+            arrowFireUI.color = newColor;
+            arrowHookUI.color = newColor;
+        }
+    }
 }
