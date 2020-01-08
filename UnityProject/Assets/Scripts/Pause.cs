bb009a4efda15f44cb0363c42965e4a48b512c99
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2020-01-08 13:27:27 +0100

    Pause Menu Added

---
+++ UnityProject/Assets/Scripts/Pause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    
+    bool displayed;
+
+    void Start()
+    {        
+        pauseMenu.SetActive(false);
+        displayed = false;        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            DisplayMenu();
+        }            
+    }
+
+    void DisplayMenu()
+    {
+        if (displayed)
+        {
+            pauseMenu.SetActive(false);
+
+            Time.timeScale = 1f;            
+        }            
+        else
+        {
+            pauseMenu.SetActive(true);
+
+            Time.timeScale = 0f;            
+        }
+                    
+        displayed = !displayed;        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
