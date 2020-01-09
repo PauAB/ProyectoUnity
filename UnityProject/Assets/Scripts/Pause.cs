85964ffb91f0dc90817ee5cc7469b76cf681160f
Pau Andreotti Bosch <44765708+PauAB@users.noreply.github.com>
2020-01-09 18:23:59 +0100

    Final Build

--- UnityProject/Assets/Scripts/Pause.cs
+++ UnityProject/Assets/Scripts/Pause.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
     public Camera cam;
-    
+    public AudioSource[] audioSources;
+
+    public float vol1;
+    public float vol2;
+    public float vol3;
+    public float vol4;
+    public float vol5;
+
     bool displayed;
 
     void Start()
     {        
         pauseMenu.SetActive(false);
-        displayed = false;        
+        displayed = false;
+
+        vol1 = audioSources[0].volume;
+        vol2 = audioSources[1].volume;
+        vol3 = audioSources[2].volume;
+        vol4 = audioSources[3].volume;
+        vol5 = audioSources[4].volume;
     }
     
     void Update()
@@ -26,27 +38,33 @@ public class Pause : MonoBehaviour
 
     void DisplayMenu()
     {
-        if (displayed)
+        if (displayed) // UNPAUSE
         {
             pauseMenu.SetActive(false);
 
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
-            //cam.cullingMask |= 1 << LayerMask.NameToLayer("");
-            //cam.cullingMask &= ~(1 << LayerMask.NameToLayer("UI"));
+            audioSources[0].volume = vol1;
+            audioSources[1].volume = vol2;
+            audioSources[2].volume = vol3;
+            audioSources[3].volume = vol4;
+            audioSources[4].volume = vol5;
 
-            Time.timeScale = 1f;            
+            Time.timeScale = 1f;
         }            
-        else
+        else // PAUSE
         {
             pauseMenu.SetActive(true);
 
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
 
-            //cam.cullingMask |= 1 << LayerMask.NameToLayer("UI");
-            //cam.cullingMask &= ~(1 << LayerMask.NameToLayer("Gun"));
+            foreach (AudioSource source in audioSources)
+            {
+                if (source.isPlaying)
+                    source.volume = 0f;
+            }
 
             Time.timeScale = 0f;            
         }
